00e013f54c42e9168d0d79667f77bea5c433289f
theangelofdead01 <rockinthecity142@gmail.com>
2022-07-26 20:50:25 -0500

    Alioth: evox bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
 # Copyright (C) 2021-2022 The LineageOS Project
 # Copyright (C) 2022 VoidUI Project
+
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_alioth.mk
+    $(LOCAL_DIR)/evolution_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_alioth-user \
-    aosp_alioth-userdebug \
-    aosp_alioth-eng
+    evolution_alioth-userdebug \
+    evolution_alioth-eng
