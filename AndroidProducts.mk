d60829bd11ca672eb0b739480d9a93584044a169
theangelofdead01 <rockinthecity142@gmail.com>
2022-08-10 12:05:22 -0500

    alioth: ricedroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,12 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
-# Copyright (C) 2022 VoidUI Project
-
+# Copyright (C) 2021 The xdroidOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_alioth.mk
+    $(LOCAL_DIR)/lineage_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_alioth-userdebug \
-    evolution_alioth-eng
+    lineage_alioth-userdebug \
+    lineage_alioth-eng
