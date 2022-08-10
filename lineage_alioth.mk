2a85a5e984e8a6dc98653c66df903939aaddfc8d
theangelofdead01 <rockinthecity142@gmail.com>
2022-08-10 12:05:22 -0500

    alioth: ricedroid bringup

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -9,19 +9,31 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := evolution_alioth
-TARGET_BOOT_ANIMATION_RES := 1080
-XDROID_BOOT := 1080
+PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-XDROID_MAINTAINER := theangelofdead01
+
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := maintainer
+RICE_CHIPSET := SM8250
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := true
+
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 TARGET_SUPPORTS_QUICK_TAP := true
 
