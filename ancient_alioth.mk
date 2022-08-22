1c47257464939a22d787ca707e23cc5af5bae344
johnmart19 <johnivan19999@gmail.com>
2022-08-22 03:48:04 +0300

    Alioth: Inherit Updatable Google Apex
    https://github.com/TheParasiteProject/vendor_pixel-additional/

--- ancient_alioth.mk
+++ ancient_alioth.mk
@@ -11,6 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
+# Updatable APEX
+TARGET_SHIP_PREBUILT_APEX := true
+$(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
+
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
