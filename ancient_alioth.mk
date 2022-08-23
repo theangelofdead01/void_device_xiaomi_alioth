f4799285cf93f32c57a6e518b77cfa5237dd3d08
theangelofdead01 <rockinthecity142@gmail.com>
2022-08-23 12:14:51 -0500

    Revert "Alioth: Include Pixel CarrierSettings"
    This reverts commit 74c4577265f92fe662edfb8dfb8245e49470e644.

--- ancient_alioth.mk
+++ ancient_alioth.mk
@@ -13,7 +13,6 @@ $(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Updatable APEX
 TARGET_SHIP_PREBUILT_APEX := true
-TARGET_INCLUDE_CARRIER_SETTINGS := true
 $(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
 
 # Inherit from alioth device
