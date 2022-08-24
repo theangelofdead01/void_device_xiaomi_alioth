1d77b2377421b844c6c6635ce28a0111732e5b88
theangelofdead01 <rockinthecity142@gmail.com>
2022-08-24 12:02:11 -0500

    alioth: SuperiorOS bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,13 @@
 #
-# Copyright (C) 2021 The xdroidOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_alioth.mk
+    $(LOCAL_DIR)/superior_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_alioth-userdebug \
-    ancient_alioth-eng
+    superior_alioth-user \
+    superior_alioth-userdebug \
+    superior_alioth-eng
--- superior_alioth.mk
+++ superior_alioth.mk
@@ -8,35 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
-
-# Updatable APEX
-TARGET_SHIP_PREBUILT_APEX := true
-TARGET_INCLUDE_CARRIER_SETTINGS := true
-$(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
-
-PRODUCT_NAME := ancient_alioth
+TARGET_USES_BLUR := true
+#USE_PIXEL_CHARGING := true
+#USE_AOSP_CLOCK := true
+PRODUCT_NAME := superior_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-
-# Gapps
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-
-# Inherit some AncientOS stuff.
-TARGET_FACE_UNLOCK_SUPPORTED := true
+BUILD_WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
-ANCIENT_OFFICIAL := true
-ANCIENT_GAPPS := true
-ANCIENT_WEEABO := true
-FORCE_LAWNCHAIR=true
-TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# SuperiorOS
+#SUPERIOR_OFFICIAL := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
