8808ed3417b7a43da302f3df286e917e7684a339
theangelofdead01 <9635187+theangelofdead01@users.noreply.github.com>
2022-08-27 11:53:15 -0500

    alioth: evox A13

--- evolution_alioth.mk
+++ evolution_alioth.mk
@@ -9,28 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := ancient_alioth
+PRODUCT_NAME := evolution_alioth
+TARGET_BOOT_ANIMATION_RES := 1080
+XDROID_BOOT := 1080
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
+XDROID_MAINTAINER := theangelofdead01
 
-# Gapps
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-
-# Inherit some AncientOS stuff.
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
-ANCIENT_OFFICIAL := true
-ANCIENT_GAPPS := true
-ANCIENT_WEEABO := true
-FORCE_LAWNCHAIR=true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
