199d5c581bd5d849afda75c87b04e5a6618bb83d
theangelofdead01 <rockinthecity142@gmail.com>
2022-09-03 12:08:03 -0500

    alioth: Elixir Welcome

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -9,21 +9,39 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := evolution_alioth
-TARGET_BOOT_ANIMATION_RES := 1080
+# Official
+ELIXIR_BUILD_TYPE := OFFICIAL
+ELIXIR_MAINTAINER := theangelofdead01
+
+PRODUCT_NAME := aosp_alioth
 XDROID_BOOT := 1080
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-XDROID_MAINTAINER := theangelofdead01
 
+#Elixir Stuff
+IS_PHONE := true
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_SYSTEM_NAME := POCOF3
+PRODUCT_SYSTEM_DEVICE := POCOF3
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
