4d7143be7bb511bb55d2146de5afcec5a3fbf420
theangelofdead01 <rockinthecity142@gmail.com>
2022-09-08 14:57:46 -0500

    alioth: Welcome Superior

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,14 @@
 #
-# Copyright (C) 2021 The xdroidOS Project
+# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_alioth.mk
+    $(LOCAL_DIR)/superior_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_alioth-userdebug \
-    evolution_alioth-eng
+    superior_alioth-userdebug \
+    superior_alioth-user \
+    superior_alioth-eng
--- superior_alioth.mk
+++ superior_alioth.mk
@@ -9,19 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := evolution_alioth
+PRODUCT_NAME := superior_alioth
 TARGET_BOOT_ANIMATION_RES := 1080
 XDROID_BOOT := 1080
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-XDROID_MAINTAINER := theangelofdead01
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 TARGET_SUPPORTS_QUICK_TAP := true
 
