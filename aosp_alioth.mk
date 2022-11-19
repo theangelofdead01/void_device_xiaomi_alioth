dae357cae3f2d9227108ea40928bbf3e944716fd
theangelofdead01 <rockinthecity142@gmail.com>
2022-11-19 15:57:35 -0500

    alioth: Elixir Bring-up

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -1,6 +1,6 @@
+
 #
-# Copyright (C) 2021-2022 The LineageOS Project
-# Copyright (C) 2022 VoidUI Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,33 +9,42 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common VoidUI stuff.
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Device Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1440
-
-# VoidUI Environment Flags
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP  := true
-TARGET_USES_MINI_GAPPS := false
-
-# VoidUI Maintainer Flags
-VOID_MAINTAINER := johnmart19
-CUSTOM_BUILD_TYPE := OFFICIAL
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
+# Official
+ELIXIR_BUILD_TYPE := OFFICIAL
+ELIXIR_MAINTAINER := theangelofdead01
+
 PRODUCT_NAME := aosp_alioth
+XDROID_BOOT := 1080
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
+#Elixir Stuff
+IS_PHONE := true
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_SYSTEM_NAME := POCOF3
+PRODUCT_SYSTEM_DEVICE := POCOF3
+
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
 
 BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.6.0.SKHMIXM:user/release-keys
