3578a8ad07f39f6a9da999752647ae2ab41d6aa4
theangelofdead01 <rockinthecity142@gmail.com>
2022-11-19 15:57:35 -0500

    alioth: Elixir Bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,5 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    aosp_alioth-userdebug \
+    aosp_alioth-user \
+    aosp_alioth-eng
